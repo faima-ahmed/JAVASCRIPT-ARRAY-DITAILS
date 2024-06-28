5fa7233a16a8572dd3c79d3ff4b2c10ebb5e9817
faima-ahmed <faimaahmeddiya@gmail.com>
2024-06-29 02:58:29 +0600

    Commit 1

--- map.js
+++ map.js
@@ -1,15 +1,27 @@
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
+// const currencies = new Map([
+//   ["USD", "United States dollar"],
+//   ["EUR", "Euro"],
+//   ["GBP", "Pound sterling"],
+// ]);
 
-currencies.forEach(function (value, key, map) {
-  console.log(`${key}: ${value}`);
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, value, map) {
+//   console.log(`${value}: ${value}`);
+// });
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUSD = 1.1;
+
+const movementsUSD = movements.map(function (mov) {
+  return mov * eurToUSD;
 });
 
-const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
-console.log(currenciesUnique);
-currenciesUnique.forEach(function (value, value, map) {
-  console.log(`${value}: ${value}`);
-});
+console.log(movements);
+
+console.log(movementsUSD);
