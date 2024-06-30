b9a8e7a09e202cbe072053ccd94b562d28bb22a6
faima-ahmed <faimaahmeddiya@gmail.com>
2024-06-30 10:10:30 +0600

    Commit 1

--- challenge.js
+++ challenge.js
@@ -3,6 +3,17 @@ const checkDogs = function (dogsJulia, dogsKate) {
   dogsJuliaCorrected.splice(0, 1);
   dogsJuliaCorrected.splice(-2);
   console.log(dogsJuliaCorrected);
+
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dogs);
+
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog Number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog Number ${i + 1} is still a puppy 🐶`);
+    }
+  });
 };
 
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
