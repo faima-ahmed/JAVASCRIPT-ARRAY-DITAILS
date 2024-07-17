da52dac0e1a2bc2af0ad525b5ce8ef375761d0ef
faima-ahmed <faimaahmeddiya@gmail.com>
2024-07-18 04:12:40 +0600

    Commit 1

--- map.js
+++ map.js
@@ -15,13 +15,26 @@
 // });
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 const eurToUSD = 1.1;
 
-const movementsUSD = movements.map(function (mov) {
-  return mov * eurToUSD;
-});
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUSD;
+// });
+
+const movementsUSD = movements.map((mov) => mov * eurToUSD);
 
 console.log(movements);
-
 console.log(movementsUSD);
+
+//for of loop
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUSD);
+console.log(movementsUSDfor);
+
+const movementsDescription = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? "deposit" : "withdrew"} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDescription);
