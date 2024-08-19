3eda337947ee7fdd0c17922f445303f538ecfd03
faima-ahmed <faimaahmeddiya@gmail.com>
2024-08-19 09:55:23 +0600

    Commit 1

---
+++ challenge.js
@@ -0,0 +1,8 @@
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  console.log(dogsJuliaCorrected);
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
