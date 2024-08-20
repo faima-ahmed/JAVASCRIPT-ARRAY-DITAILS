eebe96807ad500530f8b739cf5d30ceeb51b7db1
faima-ahmed <faimaahmeddiya@gmail.com>
2024-08-20 18:04:26 +0600

    array-loop

---
+++ at-metod.js
@@ -0,0 +1,13 @@
+//at method
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log("Diya".at(0));
+console.log("Diya".at(-1));
+console.log("Akash".at(0));
---
+++ lopping-arrays-foreach.js
@@ -0,0 +1,10 @@
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//for of loop
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
