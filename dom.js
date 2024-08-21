5c5b9c77263f0768cfb379e46bbea0e602800659
faima-ahmed <faimaahmeddiya@gmail.com>
2024-08-21 19:47:48 +0600

    Commit 1

--- dom.js
+++ dom.js
@@ -1,17 +1,2 @@
-btnLoan.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  const amount = Number(inputLoanAmount.value);
-
-  if (
-    amount > 0 &&
-    currentAccount.movements.some((mov) => mov >= amount * 0.1)
-  ) {
-    // Add movement
-    currentAccount.movements.push(amount);
-
-    // Update UI
-    updateUI(currentAccount);
-  }
-  inputLoanAmount.value = "";
-});
+const a = 10;
+console.log(a);
